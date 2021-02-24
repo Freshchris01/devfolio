7d50f396448fbd3442d18fc712c748cc8d0896cf
Christian Diemers <christian.diemers@gmail.com>
2021-02-24 16:44:08 +0100

    Add base personal information

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,19 +2,19 @@ module.exports = {
   pathPrefix: '/home',
   siteMetadata: {
     // Site URL for when it goes live
-    siteUrl: `https://elastic-meninsky-aa7c74.netlify.app/`,
+    siteUrl: `https://https://freshchris01.github.io/home/`
     // Your Name
-    name: 'Ryan Fitzgerald',
+    name: 'Christian Diemers',
     // Main Site Title
-    title: `Ryan Fitzgerald | Full-Stack Developer`,
+    title: `Christian Diemers | Software Engineer`,
     // Description that goes under your name in main bio
     description: `Lorem ipsum dolor sit amet consectetur adipisicing elit, ipsum.`,
     // Optional: Twitter account handle
     author: `@rfitzio`,
     // Optional: Github account URL
-    github: `https://github.com/ryanfitzgerald`,
+    github: `https://github.com/Freshchris01`,
     // Optional: LinkedIn account URL
-    linkedin: `https://www.linkedin.com/in/ryanafitzgerald/`,
+    linkedin: `https://www.linkedin.com/in/christian-diemers`,
     // Content of the About Me section
     about: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus perferendis porro cumque ea error ab voluptatem. Temporibus adipisci exercitationem similique itaque quibusdam laudantium, qui molestiae quas, aut amet animi id.`,
     // Optional: List your projects, they must have `name` and `description`. `link` is optional.
