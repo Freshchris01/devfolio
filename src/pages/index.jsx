0eea4593531e346720e3291f00a3ab4eeae080b3
Christian Diemers <christian.diemers@gmail.com>
2021-03-06 22:23:41 +0100

    Add education

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -9,6 +9,7 @@ import SectionBlog from '../components/section-blog';
 import SectionExperience from '../components/section-experience';
 import SectionProjects from '../components/section-projects';
 import SectionSkills from '../components/section-skills';
+import SectionEducation from '../components/section-education';
 import SEO from '../components/seo';
 
 const Index = ({ data }) => {
@@ -17,6 +18,7 @@ const Index = ({ data }) => {
   const posts = data.allMarkdownRemark.edges;
   const experience = get(data, 'site.siteMetadata.experience', false);
   const skills = get(data, 'site.siteMetadata.skills', false);
+  const education = get(data, 'site.siteMetadata.education', false);
   const noBlog = !posts || !posts.length;
 
   return (
@@ -29,6 +31,7 @@ const Index = ({ data }) => {
       {experience && experience.length && (
         <SectionExperience experience={experience} />
       )}
+      {education && education.length && <SectionEducation education={education} />}
       {skills && skills.length && <SectionSkills skills={skills} />}
     </Layout>
   );
@@ -57,6 +60,10 @@ export const pageQuery = graphql`
           description
           link
         }
+	education {
+	  name
+	  description
+	}
         skills {
           name
           description
